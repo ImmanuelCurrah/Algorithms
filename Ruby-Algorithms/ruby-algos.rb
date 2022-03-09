c8b3315f1aee284e12413cf6a1303367737f4462
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-08 20:30:08 -0500

    ruby algo solved

--- Ruby-Algorithms/ruby-algos.rb
+++ Ruby-Algorithms/ruby-algos.rb
@@ -1,7 +1,19 @@
+# convert string to camel case
 # https://www.codewars.com/kata/517abf86da9663f1d2000003/train/ruby
 
 def to_camel_case(str)
   result = []
   str == "" ? "" : str.include?("_") ? result = str.split("_") : str.include?("-") ? result = str.split("-") : nil
   result.each {|a| result.index(a) == 0 ? a : a.capitalize!}.join("")
+end
+
+# The Hashtage Generator
+# https://www.codewars.com/kata/52449b062fb80683ec000024/train/ruby
+
+def generateHashtag(str)
+  result = []
+  result = str.split(" ")
+  result.insert(0, "#")
+  final = result.each {|a| a.capitalize!}.join("")
+  final == "#" || final.length > 140  ? false : final
 end
